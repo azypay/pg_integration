9948d180a767271f36d3a0ea71335e14e8f6e912
Priya <hari@ultiweb.in>
2020-01-11 00:10:19 +0530

    pg_integ_sdk_with_eg

---
+++ app/src/main/java/com/test/pg/secure/sampleapp/SampleAppConstants.java
@@ -0,0 +1,36 @@
+package com.test.pg.secure.sampleapp;
+
+public class SampleAppConstants {
+    //API_KEY is given by the Payment Gateway. Please Copy Paste Here.
+    public static final String PG_API_KEY = "You API key";
+
+    //URL to Accept Payment Response After Payment. This needs to be done at the client's web server.
+    public static final String PG_RETURN_URL = "www.azypay.com";
+
+    //Enter the Mode of Payment Here . Allowed Values are "LIVE" or "TEST".
+    public static final String PG_MODE = "TEST";
+
+    //PG_CURRENCY is given by the Payment Gateway. Only "INR" Allowed.
+    public static final String PG_CURRENCY = "INR";
+
+    //PG_COUNTRY is given by the Payment Gateway. Only "IND" Allowed.
+    public static final String PG_COUNTRY = "IND";
+
+    public static final String PG_AMOUNT = "10";
+    public static final String PG_EMAIL = "fdhaskjdfh@gmail.com";
+    public static final String PG_NAME = "test";
+    public static final String PG_PHONE = "8333873777";
+    public static       String PG_ORDER_ID = "";
+    public static final String PG_DESCRIPTION = "hv;hghkjgg";
+    public static final String PG_CITY = "xvgf";
+    public static final String PG_STATE = "Andhra Pradesh";
+    public static final String PG_ADD_1 = "dsfg";
+    public static final String PG_ADD_2 = "xzdvdf";
+    public static final String PG_ZIPCODE = "522001";
+    public static final String PG_UDF1 = "";
+    public static final String PG_UDF2 = "";
+    public static final String PG_UDF3 = "";
+    public static final String PG_UDF4 = "";
+    public static final String PG_UDF5 = "";
+
+}
